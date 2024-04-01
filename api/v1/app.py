87809fc15ddcce6b328d2e3bba6d7d7a07d099dc
Muhadev <muhammedfayemi@gmail.com>
2024-04-01 09:25:37 -0700

    task is added

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,33 @@
-from flask import Flask
+#!/usr/bin/python3
+""" app.py file """
+import os
 from models import storage
+from flask import Flask, jsonify
 from api.v1.views import app_views
+from flask_cors import CORS
 
-# Create a Flask instance
+
 app = Flask(__name__)
-
-# Register the blueprint app_views to your Flask instance app
+db = os.environ.get('HBNB_TYPE_STORAGE', 'json_file')
+host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+port = os.environ.get('HBNB_API_PORT', '5000')
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-# Declare a method to handle @app.teardown_appcontext storage.close()
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(error):
+def close(cls):
+    """close"""
     storage.close()
 
 
-if __name__ == "__main__":
-    import os
-    # Define host and port
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+@app.errorhandler(404)
+def not_found(e):
+    """error 404"""
+    return jsonify({"error": "Not found"}), 404
 
-    app.run(host=host, port=port, threaded=True)
+
+if __name__ == '__main__':
+    """Initialize api"""
+    app.run(host=host, port=int(port), threaded=True)
