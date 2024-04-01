dc7d477a4b264e37720102de0d54f5ee91768003
Muhadev <muhammedfayemi@gmail.com>
2024-04-01 09:20:26 -0700

    task is added

--- api/v1/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Create a Blueprint instance with url prefix /api/v1
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Wildcard import (PEP8 will complain about this, ignore)
-from api.v1.views.index import *
